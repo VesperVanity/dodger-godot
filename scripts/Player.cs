300ca6dd14f5e4877e3dbc20623a0c60095175b2
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-07 17:44:00 -0500

    Add simple player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : Area2D
 {
@@ -9,10 +10,23 @@ public partial class Player : Area2D
 
 	}
 
-	
 	public override void _Process(double delta)
 	{
-
+		player_movement(delta);
 	}
-	
+
+	private void player_movement(double delta_time)
+	{
+		float player_speed = 700.0f;
+		float player_movement = player_speed * (float)delta_time;
+		if(Input.IsActionPressed("left"))
+		{
+			Position += new Vector2(-player_movement, 0.0f);
+		}
+		else if(Input.IsActionPressed("right"))
+		{
+			Position += new Vector2(player_movement, 0.0f);
+		}
+	}
+
 }
